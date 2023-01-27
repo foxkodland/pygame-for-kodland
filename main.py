d5cf71f610663046c271bf7099c238a38157d24e
Pavel <102648390+foxkodland@users.noreply.github.com>
2023-01-27 18:07:42 +0300

    update main
    удалил комментарии

--- main.py
+++ main.py
@@ -5,12 +5,8 @@ WIDTH = 600  # ширина игрового окна
 HEIGHT = 480 # высота игрового окна
 FPS = 30 # частота кадров в секунду
 
-# Цвета (R, G, B)
+# Цвет
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
